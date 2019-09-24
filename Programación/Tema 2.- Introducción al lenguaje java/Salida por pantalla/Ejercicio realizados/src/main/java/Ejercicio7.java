dbcda93d822d74983d215b08f604c0094ab2a0e2
Chirili <chirili503@gmail.com>
2019-09-24 17:33:25 +0200

    fixed project maven

---
+++ Programación/Tema 2.- Introducción al lenguaje java/Salida por pantalla/Ejercicio realizados/src/main/java/Ejercicio7.java
@@ -0,0 +1,14 @@
+public class Ejercicio7 {
+    public void piramideHueca() {
+        /*
+        *
+        * Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe verúnicamente el contorno hecho con asteriscos).
+        *
+        * */
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *     *");
+        System.out.println("*********");
+    }
+}
